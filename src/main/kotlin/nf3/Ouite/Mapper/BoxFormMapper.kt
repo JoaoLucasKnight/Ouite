aeb9c0c26cfb6f1668bf710de10eff39a5d157e8
JoaoLucasKnight <64169352+JoaoLucasKnight@users.noreply.github.com>
2024-03-04 11:23:27 -0300

    fazeendo o CRUD do box (incompleto)

--- src/main/kotlin/nf3/Ouite/Mapper/BoxFormMapper.kt
+++ src/main/kotlin/nf3/Ouite/Mapper/BoxFormMapper.kt
@@ -2,16 +2,34 @@ package nf3.Ouite.Mapper
 
 import nf3.Ouite.DTO.BoxForm
 import nf3.Ouite.Model.Box
+import nf3.Ouite.Model.Space
+import nf3.Ouite.Service.SpaceService
+import nf3.Ouite.repository.BoxRepository
+import nf3.Ouite.repository.SpaceRepository
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.stereotype.Component
 
-class BoxFormMapper: Mapper <BoxForm, Box>{
+@Component
+class BoxFormMapper(
+    private val boxRepos: BoxRepository,
+    private val spaceRepos: SpaceRepository
+): Mapper <BoxForm, Box>{
     override fun map(b: BoxForm): Box {
+
+        val spacePai: Space = b.space_id.let{
+            spaceRepos.findById(it).orElseThrow{NotFoundException()}
+        }
+
+        val boxPai: Box? = b.box_id?.let { boxRepos
+            .findById(it).orElse(null) }
+
+
         return Box (
             nome = b.nome,
             descricao = b.descricao,
             caminho = b.caminho,
-            space = b.space_id,
-            boxPai = b.box_id
-            
+            space = spacePai,
+            boxPai = boxPai
 
         )
     }
